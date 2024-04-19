2c4f8d404755164d54796353d98c677095940e8c
alexander.generalov <49165427+AlexanderGeneralov@users.noreply.github.com>
2024-04-19 08:37:54 +0300

    Update reveal_number.py more cleare code

--- strings/reveal_number.py
+++ strings/reveal_number.py
@@ -5,21 +5,18 @@ def reveal_num(line: str):
     dot = False
     
     for char in line:
-        if char in "+-" and not res:
-            sign = "-"*(char == "-")
-"""boolean b = (char == "-") will evaluate to b = 0 (False) or b = 1 (True)
-in this context so that "-"*b will be either "" 
-(in this case no need to redefine since sign is already the empty string) or "-""""            
+        if char == "-":
+            sign = "-"
         elif char in "." and not dot:
             dot = True
             res += "."
         elif char.isdigit():
             res += char
-"""Explanation: (int, float) creates a tuple. 
-boolean dot will again evaluate as an integer in this context to grab the 0th (False) or 1st (True) element of that tuple, 
-which is a string to number conversion function in either case, to finally apply it to sign + res"""        
-    if res:
-        return(int, float)[dot](sign + res)
+
+    if dot:
+        return float(sign + res)
+    else:
+        return int(sign + res)
 
 
 print("Example:")
